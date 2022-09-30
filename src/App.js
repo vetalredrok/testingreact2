9e88fe5d4007ca651956c955ee8b5abef8701deb
vetalredrok <vetalredrok@gmail.com>
2022-09-30 16:34:47 +0300

    reducer-part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from "react";
+
+const init = (initialValue) => {
+  return {counter1: initialValue, counter2: initialValue}
+};
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case 'INC1':
+            return {...state, counter1: state.counter1+1}
+        case 'DEC1':
+            return {...state, counter1: state.counter1-1}
+        case 'RESET1':
+            return {...state, counter1:0}
+        case 'SET1':
+            return {...state, counter1: action.payload}
+    }
+  return {...state}
+}
+
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, 0, init);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>counter1: {state.counter1}</h1>
+        <h1>counter2: {state.counter2}</h1>
+        <button onClick={()=>dispatch({type: 'INC1'})}>inc</button>
+        <button onClick={()=>dispatch({type: 'DEC1'})}>dec</button>
+        <button onClick={()=>dispatch({type: 'RESET1'}) }>reset</button>
+        <button onClick={()=>dispatch({type: 'SET1', payload:10})}>set</button>
     </div>
   );
 }
