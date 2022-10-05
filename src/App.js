d36ceb39871bbf93490c13f6aa3a14dcb943f3d2
vetalredrok <vetalredrok@gmail.com>
2022-10-05 18:22:05 +0300

    logination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
+
+  const state = useSelector(state => state.userReducer);
+  console.log(state);
+  let dispatch = useDispatch();
+
+  useEffect(()=>{
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(value => {
+          dispatch({type: 'LOAD_USERS', payload: value});
+        })
+
+
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,61 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+
+
+// let initState = {users: [], user: null, posts: []};
+
+const userReducer = (state = {users: [], user: null}, action)=> {
+
+    switch (action.type){
+        case "LOAD_USERS":
+            return {...state, users:action.payload};
+
+        case "CHOOSE_USER":
+            let id = action.payload;
+            let user = state.users.find(value => value.id === id);
+            return {...state, user:user}
+
+        default:
+            return state;
+    }
+}
+
+const postReducer = (state = {posts: [], post: null}, action)=> {
+
+    switch (action.type){
+        case "LOAD_POSTS":
+            let payload = action.payload;
+            return {...state, posts: payload}
+
+        case "CHOOSE_POST":
+            let id = action.payload;
+            let post = state.posts.find(value => value.id === id);
+            return {...state, post:post}
+
+        default:
+            return state;
+    }
+}
+
+let combineReducer = combineReducers({
+    userReducer,
+    postReducer
+});
+
+
+
+ let store = createStore(combineReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
